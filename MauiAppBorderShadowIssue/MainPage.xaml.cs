472e2a0df38f7c3150630c5ba5a49193cf422516
Jeroen Corteville <jeroen.corteville@proplanner.eu>
2025-04-24 13:40:25 +0200

    added button for showcasing issue better

--- MauiAppBorderShadowIssue/MainPage.xaml.cs
+++ MauiAppBorderShadowIssue/MainPage.xaml.cs
@@ -11,17 +11,15 @@
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            IncreaseCount();
         }
 
         private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+        {
+            IncreaseCount();
+        }
+
+        private void IncreaseCount()
         {
             count++;
 
